6d36131e259a2c43b3ad090222dc549b8108638a
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-10 12:08:03 +0100

    Implemented the TestIllegalRuleNumber test

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,27 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
 class TestDeathNote {
 
+    private static final int NEGATIVE_NUMBER = -1;
+
+    @Test
+    void testIllegalRuleNumber() {
+        final DeathNoteImpl deathNote = new DeathNoteImpl();
+        try {
+            deathNote.getRule(0);
+            deathNote.getRule(NEGATIVE_NUMBER);
+            fail("Getting an negative or 0 rule was possible, but should have thrown an exception");
+        } catch (final IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+        }
+    }
 }
