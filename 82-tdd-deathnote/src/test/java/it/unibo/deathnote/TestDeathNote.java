a721b5f9745e8269f937664b9d32003796cea43d
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-10 12:53:19 +0100

    Implemented the testWrite test

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,6 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,7 +14,10 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 class TestDeathNote {
 
     private static final int NEGATIVE_NUMBER = -1;
-    private static final String HUMAN_NAME = "Marco";
+    private static final String HUMAN_NAME_1 = "Marco";
+    private static final String HUMAN_NAME_2 = "Giorgio";
+    private static final String DEATH_CAUSE = "karting accident";
+    private static final long SLEEP_TIME = 100;
 
     @Test
     void testIllegalRuleNumber() {
@@ -39,10 +43,30 @@ class TestDeathNote {
     @Test
     void testWrite() {
         final DeathNoteImpl deathNote = new DeathNoteImpl();
-        assertFalse(deathNote.isNameWritten(HUMAN_NAME));
-        deathNote.writeName(HUMAN_NAME);
-        assertTrue(deathNote.isNameWritten(HUMAN_NAME));
+        assertFalse(deathNote.isNameWritten(HUMAN_NAME_1));
+        deathNote.writeName(HUMAN_NAME_1);
+        assertTrue(deathNote.isNameWritten(HUMAN_NAME_1));
         assertFalse(deathNote.isNameWritten("Mario"));
         assertFalse(deathNote.isNameWritten(""));
+    }
+
+    @Test
+    void testDeathCause() throws InterruptedException {
+        final DeathNoteImpl deathNote = new DeathNoteImpl();
+        try {
+            deathNote.writeDeathCause(HUMAN_NAME_1);
+            fail("Writing a death cause without any name in the death note was possible, but should have thrown an exception");
+        } catch (final IllegalStateException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+        }
+        deathNote.writeName(HUMAN_NAME_1);
+        assertEquals("heart attack", deathNote.getDeathCause(HUMAN_NAME_1));
+        deathNote.writeName(HUMAN_NAME_2);
+        assertTrue(deathNote.writeDeathCause(DEATH_CAUSE));
+        assertEquals(DEATH_CAUSE, deathNote.getDeathCause(HUMAN_NAME_2));
+        Thread.sleep(SLEEP_TIME);
+        deathNote.writeDeathCause("heart attack");
+        assertEquals(DEATH_CAUSE, deathNote.getDeathCause(HUMAN_NAME_2));
     }
 }
