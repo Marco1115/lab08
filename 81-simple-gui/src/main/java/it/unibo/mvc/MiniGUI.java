b247d07197706648a71db6b966242ec579bcf4e6
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-10 11:05:16 +0100

    Implemented the second part of exercise 81

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -4,6 +4,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -37,6 +39,8 @@ public class MiniGUI {
         canvas.add(innerPanel, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
         innerPanel.add(write);
+        final JTextField resultField = new JTextField("Result");
+        canvas.add(resultField, BorderLayout.NORTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
