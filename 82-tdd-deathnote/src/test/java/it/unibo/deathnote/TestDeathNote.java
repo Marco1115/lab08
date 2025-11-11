06f0e12aeba97e36ff1f75966577213a138cbea3
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-11 17:02:50 +0100

    Corrected testIllegalRuleNumber so that checks also for all three cases: negative, zero and too large number

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,6 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 
 class TestDeathNote {
 
-    private static final int NEGATIVE_NUMBER = -1;
     private static final String HUMAN_NAME_1 = "Marco";
     private static final String HUMAN_NAME_2 = "Giorgio";
     private static final String DEATH_CAUSE = "karting accident";
@@ -31,13 +32,14 @@ class TestDeathNote {
 
     @Test
     void testIllegalRuleNumber() {
-        try {
-            deathNote.getRule(0);
-            deathNote.getRule(NEGATIVE_NUMBER);
-            fail("Getting an negative or 0 rule was possible, but should have thrown an exception");
-        } catch (final IllegalArgumentException e) {
-            assertNotNull(e.getMessage());
-            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+        for (final int i: List.of(-1, 0, DeathNote.RULES.size() + 1)) {
+            try {
+                deathNote.getRule(i);
+                fail("Getting a rule with an illegal number was possible, but should have thrown an exception");
+            } catch (final IllegalArgumentException e) {
+                assertNotNull(e.getMessage());
+                assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+            }
         }
     }
 
