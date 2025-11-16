02893c4b1797f646fa4b040fdba09e03002a8437
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-16 16:41:37 +0100

    Created theDrawNumberStandardOutputView class

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStandardOutputView.java
@@ -0,0 +1,30 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+/**
+ * Command line {@link DrawNumberView} implementation.
+ */
+public final class DrawNumberStandardOutputView implements DrawNumberView {
+
+    @Override
+    public void setController(final DrawNumberController observer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'result'");
+    }
+
+}
